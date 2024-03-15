d1bc2e2430288d96e4c9f56d1d8347a141741094
Tanaka <102905708+neko385@users.noreply.github.com>
2024-03-15 09:56:32 +0900

    課題提出

--- src/practice/Chapter5.java
+++ src/practice/Chapter5.java
@@ -1,5 +1,24 @@
 package practice;
 
-public class Chapter5 {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class Chapter5  {
+	public static void main(String[] args) {
+		List<Task> tasks = new ArrayList<>();
+		
+		tasks.add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う。"));
+		tasks.add(new Task(LocalDate.of(2021, 9, 15), "○○社面談。"));
+		tasks.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う。"));
+		tasks.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く。"));
+		tasks.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く。"));
+		
+		Collections.sort(tasks, (t1, t2) -> t1.getDate().compareTo(t2.getDate()));
+		
+		for(Task task : tasks) {
+			System.out.println(task);
+		}
+	}
 }
--- src/practice/Task.java
+++ src/practice/Task.java
@@ -1,5 +1,27 @@
 package practice;
 
-public class Task {
+import java.time.LocalDate;
 
+public class Task {
+	private LocalDate date;
+	private String content;
+	
+	public Task(LocalDate date, String content) {
+		this.date = date;
+		this.content = content;
+	}
+	
+	public LocalDate getDate() {
+		return date;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	@Override
+	public String toString() {
+		return this.date + ":" + this.content;
+	}
+	
 }
